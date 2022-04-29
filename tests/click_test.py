10f4595c32f8db2e8c00d477e75338855544e107
ounmuhammad32 <45170041+ounmuhammad32@users.noreply.github.com>
2022-04-29 11:22:17 -0400

    aa

--- tests/click_test.py
+++ tests/click_test.py
@@ -1,4 +1,4 @@
-import logging
+""" Test Click interface """
 import os
 
 from click.testing import CliRunner
@@ -8,13 +8,12 @@ from app import create_database
 runner = CliRunner()
 
 
-
-
 def test_create_database():
+    """ test database folder creation """
     response = runner.invoke(create_database)
     assert response.exit_code == 0
     root = os.path.dirname(os.path.abspath(__file__))
     # set the name of the apps log folder to logs
     dbdir = os.path.join(root, '../database')
     # make a directory if it doesn't exist
-    assert os.path.exists(dbdir) == True
+    assert os.path.exists(dbdir)
